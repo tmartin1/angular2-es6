7c4aae1b673d172e26a7cb6794fd4167afa13b91
Tim Martin <tmartin1@users.noreply.github.com>
2015-10-20 17:37:27 -0400

    Update app.es6.js

--- app.es6.js
+++ app.es6.js
@@ -1,7 +1,8 @@
-import {Component, View, Control, bootstrap, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {Component, View, bootstrap, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
-  selector: 'app'
+  selector: 'app',
+  providers: [TaskService]
 })
 
 @View({
@@ -10,31 +11,51 @@ import {Component, View, Control, bootstrap, CORE_DIRECTIVES, FORM_DIRECTIVES} f
 })
 
 class App {
-  constructor() {
+  constructor(taskService: TaskService) {
     this.greeting = 'Welcome!';
-    this.tasks = this.defaultTasks();
-    setTimeout(() =>
-      this.greeting = 'To Do List (v1) - ' + moment().format('LL'), 2000);
+    setTimeout(() => this.greeting = 'To Do List (v2) - ' + moment().format('LL'), 2000);
+    
+    this.tasks = taskService.getTasks();
+    this.saveTasks = taskService.saveTasks;
   }
   
   addTask() {
     if (this.newTask.length > 0) {
       this.tasks.push({ text: this.newTask, complete: false });
-      this.newTask = '';
+      delete this.newTask;
     }
   }
   
-  removeTask(index) {
-    this.tasks.splice(index,1);
+  toggleTask(task) {
+    task.complete = !task.complete;
   }
   
-  defaultTasks() {
+  removeTask(index) {
+    this.tasks.splice(index, 1);
+  }
+}
+
+class TaskService {
+  constructor() {
+    this._tasks = this.loadTasks();
+  }
+  
+  saveTasks(tasks) {
+    this._tasks = tasks;
+    console.log('saved');
+  }
+  
+  getTasks() {
+    return this._tasks;
+  }
+  
+  loadTasks() {
+    // Let's use our $http imagination here.
     return [
       { text: 'First example task.', complete: true },
       { text: 'Second example task.' }
     ];
   }
-  
 }
 
 bootstrap(App);
