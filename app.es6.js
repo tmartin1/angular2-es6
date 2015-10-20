006d199b8abd51488ef722d0738eb932dd8b3f49
Tim Martin <tmartin1@users.noreply.github.com>
2015-10-20 17:48:47 -0400

    Update app.es6.js

--- app.es6.js
+++ app.es6.js
@@ -1,19 +1,27 @@
-import {Component, View, bootstrap, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {Component, Pipe, PipeTransform, View, bootstrap, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
   selector: 'app',
   providers: [TaskService]
 })
 
+@Pipe({
+  name: 'taskFilter'
+})
+
 @View({
   directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
+  // pipes: [taskFilter],
   templateUrl: 'main.html'
 })
 
 class App {
   constructor(taskService: TaskService) {
     this.greeting = 'Welcome!';
-    setTimeout(() => this.greeting = 'To Do List (v2) - ' + moment().format('LL'), 2000);
+    setTimeout(() => this.greeting = 'To Do List (v3) - ' + moment().format('LL'), 2000);
+    
+    this.navtabs = [ 'All Tasks', 'To Do', 'Completed' ];
+    this.currentFilter = this.navtabs[0];
     
     this.tasks = taskService.getTasks();
     this.saveTasks = taskService.saveTasks;
@@ -22,7 +30,7 @@ class App {
   addTask() {
     if (this.newTask.length > 0) {
       this.tasks.push({ text: this.newTask, complete: false });
-      delete this.newTask;
+      this.newTask = '';
     }
   }
   
@@ -32,6 +40,17 @@ class App {
   
   removeTask(index) {
     this.tasks.splice(index, 1);
+  }
+  
+  filterTasks(setting) {
+    this.currentFilter = setting;
+    console.log(this.currentFilter);
+  }
+  
+  ghettoFilter(isComplete) {
+    if (this.currentFilter === 'To Do') return !!isComplete;
+    else if (this.currentFilter === 'Completed') return !isComplete;
+    else return false;
   }
 }
 
@@ -53,8 +72,15 @@ class TaskService {
     // Let's use our $http imagination here.
     return [
       { text: 'First example task.', complete: true },
-      { text: 'Second example task.' }
+      { text: 'Second example task.', complete: false }
     ];
+  }
+}
+
+class taskFilter {
+  transform(task, args) {
+    console.log('transforming', task, args[0]);
+    return !!task.completed ? task.text : null;
   }
 }
 
